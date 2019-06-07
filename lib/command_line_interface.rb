a7d0bfa8dcf8c940410fb1784f9caba5ece9e0ba
github username <github email address>
2019-06-07 14:47:38 -0400

    fleshes out direction of CLI further

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -11,9 +11,20 @@ class CLI
       puts "Hello, basketball head! Welcome to Stat-Padder!  For a list of categories, enter 'start'"
    end
 
-   def categories
-      # puts out a list of major statistical categories in the 2019 NBA season 
+   # user enters "start"
+
+   def display_categories
+      #Category.all.sort -> returns list of major statistical categories in the 2019 NBA season (category names will be in a hash)
    end
+
+   # accepts user input here to determine which category's players they'd like to see
+
+  def display_players_and_totals
+      # Player.all.sort
+     # code here -> ultimately returns the names of the players in that category + their total in that category
+  end
+
+   
 
    
 end
