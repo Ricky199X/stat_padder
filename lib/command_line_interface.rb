f7270e83a7c6af592a74408b249c695c30214068
github username <github email address>
2019-06-07 16:03:05 -0400

    further stubs out CLI

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,26 +4,37 @@ class CLI
    def run
       self.greeting
       HomepageScraper.scrape_homepage
-      self.display_categories
+      self.start
+      # self.display_categories
    end
 
    def greeting
       puts "Hello, basketball head! Welcome to Stat-Padder!  For a list of categories, enter 'start'"
    end
 
-   # user enters "start"
+   # user enters start
 
-   def display_categories
-      #Category.all.sort -> returns list of major statistical categories in the 2019 NBA season (category names will be in a hash)
-      puts <<-DOC
-         1. Points 
-         2. Rebounds
-         3. Assists
-         4. Blocks
-         5. Steals
-         6. Minutes
-      DOC
+   def start
+      input = gets.strip
+
+      if input == "start"
+         puts <<-DOC
+            1. Points
+            2. Rebounds
+            3. Assists
+            4. Blocks
+            5. Steals
+            6. Minutes
+         DOC
+      else 
+         puts "I'm sorry, please enter 'start'."
+      end
    end
+
+   # def display_categories
+   #    #Category.all.sort -> returns list of major statistical categories in the 2019 NBA season (category names will be in a hash)
+     
+   # end
 
    # accepts user input here to determine which category's players they'd like to see
 
