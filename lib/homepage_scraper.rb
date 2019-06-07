1061f66de5bcb4417999d7a4bef78098289ad7aa
github username <github email address>
2019-06-06 22:50:13 -0400

    Hompage data collected.

--- lib/homepage_scraper.rb
+++ lib/homepage_scraper.rb
@@ -1,15 +1,21 @@
 class HomepageScraper
 
    BASE_URL = "https://basketball.realgm.com/nba/stats"
-   PLAYER_URL = ""
+   
 
    def self.scrape_homepage
+      players_array = []
       page = open(BASE_URL) # page is now returning content 
       parsed_players = Nokogiri::HTML(page)
 
-      category_names = parsed_HTML.css("div.category-name").map(&:text)
-      category_player_links = parsed_HTML.css("a").attribute("href").value
-      category_totals = parsed_HTML.css("span.stat").map(&:text)
+      category_names = parsed_players.css("div.category-name").map(&:text)
+      category_totals = parsed_players.css("span.stat").map(&:text)
+      category_player_names = parsed_players.css("span.player-name").collect do |link|
+         link.css("a").text.strip
+      end
+      category_player_links = parsed_players.css("span.player-name").collect do |link|
+         link.css("a").attribute('href').value
+      end
 
       
       binding.pry
