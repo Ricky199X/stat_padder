3fbd809be85be932f9bd4460960168279b8349ac
github username <github email address>
2019-06-08 00:18:12 -0400

    further updates scraper

--- lib/homepage_scraper.rb
+++ lib/homepage_scraper.rb
@@ -22,36 +22,36 @@ class HomepageScraper
    # end
 
    def self.scrape_categories
-      page = open(BASE_URL) # page is now returning content 
-      parsed_players = Nokogiri::HTML(page)
+      page = open(BASE_URL)
+      parsed_HTML = Nokogiri::HTML(page)
       category_array = []
-      parsed_players.css("div.overall-leader").each do |category|
+      parsed_HTML.css("div.overall-leader").each do |category|
          category_array << category.css("div.category-name").text
       end
-      category_array
+      return category_array
       # binding.pry
    end
 
    def self.scrape_player_names
-      page = open(BASE_URL) # page is now returning content 
-      parsed_players = Nokogiri::HTML(page)
+      page = open(BASE_URL)
+      parsed_HTML = Nokogiri::HTML(page)
       player_array = []
-      parsed_players.css("span.player-name").each do |link|
+      parsed_HTML.css("span.player-name").each do |link|
          player_array << link.css("a").text.strip
       end
-      player_array
+      return player_array
+      binding.pry
+   end
+
+   def self.scrape_player_urls
+      page = open(BASE_URL)
+      parsed_HTML = Nokogiri::HTML(page)
+      player_urls = []
+      parsed_HTML.css("span.player-name").each do |url|
+         player_urls << url.css("a").attribute("href").value
+      end
+      return player_urls
       # binding.pry
    end
 end
 
-# will need 6 stats objects
-
-# Stats class - needs a player array
-# can have a name + have a number
-
-# @@all in stats 
-
-# Stats <-> Player  
-# # will need find or create by methods 
-
-# As they choose a person, they'll need an array of stats for that player
