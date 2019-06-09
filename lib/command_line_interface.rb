025bee632872cbd632db19004c88be3c57ae5183
github username <github email address>
2019-06-09 09:33:52 -0400

    stubs player menus

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -30,7 +30,59 @@ class CLI
 
 
    def menu
-      puts "\n\nEnter the number of the category or the catagory name for more information."
+      puts "\n\nEnter the number of the category for more information."
+      input = gets.strip.downcase.to_i
+
+      case input
+         when 1
+               puts <<-DOC
+            1. James Harden
+            2. Paul George
+            3. Giannis Antetokounmpo
+            4. Joel embiid
+            5. Lebron James
+         DOC
+         when 2
+            puts <<-DOC
+            1. Andre Drummond
+            2. Joel embiid
+            3. Deandre Jordan
+            4. Rudy Gobert
+            5. Clint Capela
+         DOC
+         when 3
+            puts <<-DOC
+            1. Russell westbrook
+            2. john wall
+            3. kyle lowry
+            4. lebron james
+            5. jeff teague
+         DOC
+         when 4
+            puts <<-DOC
+            1. myles turner
+            2. mitchell robinson
+            3. anthony davis
+            4. rudy gobert
+            5. brook lopez
+         DOC
+         when 5
+            puts <<-DOC
+            1. paul george
+            2. robert covington
+            3. james harden
+            4. chris paul
+            5. russell westbrook
+         DOC
+         when 6 
+            puts <<-DOC
+            1. bradley beal
+            2. paul george
+            3. james harden
+            4. russell westbrook
+            5. jrue holiday
+         DOC
+      end
    end
 
    # def display_categories
