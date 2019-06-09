80223b441a15ce27fe53642f9cfd8a634df9183c
github username <github email address>
2019-06-09 13:42:24 -0400

    sets up basic player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,17 @@
-class Player
+class Category
+   # each category has a name, players, and urls
+   attr_accessor :name, :players, :player_links
+
+   @@all = []
+
+   def save
+      @@all << self
+   end
+
+
+   def self.all
+      @@all
+      # binding.pry
+   end
+
 end
