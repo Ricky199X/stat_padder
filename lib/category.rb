a9cb5970212fe4f4e1b7db994f11638e0b9f5efc
github username <github email address>
2019-06-08 23:42:22 -0400

    stubs category class

--- lib/category.rb
+++ lib/category.rb
@@ -1,7 +1,46 @@
 class Category
    # each category has a name, players, and urls
-   attr_accessor :category_name, :players, :player_links
+   attr_accessor :category_name, :players
 
+   def self.categories
+      #should return a list of categories
+      # Points
+      # Rebounds
+      # Assists
+      # Blocks
+      # Steals
+      # Minutes
+
+      # category_1 = self.new
+      # category_1.name = "Points"
+      # category_1.players = ["James Harden", "Paul George", "Giannis Antetokounmpo", "Joel Embiid", "LeBron James"]
+ 
+ 
+      # category_2 = self.new
+      # category_2.name = "Rebounds"
+      # category_2.players = ["Andre Drummond", "Joel Embiid", "DeAndre Jordan", "Rudy Gobert", "Clint Capela"]
+
+      # category_3 = self.new
+      # category_3.name = "Assists"
+      # category_3.players = ["Russell Westbrook", "John Wall", "Kyle Lowry", "LeBron James", "Jeff Teague"]
+
+      # category_4 = self.new
+      # category_4.name = "Blocks"
+      # category_4.players = ["Myles Turner", "Mitchell Robinson", "Anthony Davis", "Rudy Gobert", "Brook Lopez"]
+
+      # category_5 = self.new
+      # category_5.name = "Steals"
+      # category_5.players = ["Paul George", "Robert Covington", "James Harden", "Chris Paul", "Russell Westbrook"]
+
+      # category_6 = self.new
+      # category_6.name = "Minutes"
+      # category_6.players = ["Bradley Beal", "Paul George", "James Harden", "Russell Westbrook", "Jrue Holiday"]
+
+      # @@all = [category_1, category_2, category_3, category_4, category_5, category_6]
+
+
+
+   end
 
 
 
