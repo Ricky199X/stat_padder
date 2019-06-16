fdc94551fd9cdd6e1c7d72f3d77429e33d1377d6
github username <github email address>
2019-06-16 11:32:59 -0400

    fixes the player_bio bug

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -2,6 +2,7 @@ class CLI
    def run
    # Scrapes the data, loads everything up 
    HomepageScraper.scrape_players
+   # HomepageScraper.scrape_bio
    puts "\n"
    puts " ------------------------------------------------------------ "
    puts " ------------------------------------------------------------ "
@@ -10,7 +11,7 @@ class CLI
    puts " ------------------------------------------------------------ "
    puts " ------------------------------------------------------------ "
    puts " ------------------------------------------------------------ "
-   puts  "\nGreetings, basketball head. To see the 50 players who defined the game of basketball as we know it. Enter 'list'\n"
+   puts  "\nGreetings, basketball head. To see the 50 players who defined the game of basketball as we know it. Enter 'start'\n"
    welcome
    end
 
@@ -23,16 +24,19 @@ class CLI
          user_input = gets.strip.downcase
 
          case 
-            when user_input == "list"
-            puts "\n"
-               Player.list_players
-            puts "\nSelect a player by number:"
+            when user_input == "start"
+               puts "\nSelect a page of players you would like to view (1-10, 11-20, 21-30, 31-40,41-50)"
+               
+               input = gets.strip.to_i
+               print_players(input)
 
-            when user_input.to_i > 0 #need to also account for it being less than 50 (Kareem not returning)
+               puts "\nPick a player by number: "
+
+            when user_input.to_i > 0
             puts ""
                choose_player(user_input)
         
-            puts "To look at another player, enter a number from 1 - 50."
+            puts "To look at another player, enter a number from 1 - 50 or enter 'exit' to leave."
 
             when user_input.to_i > 0
                choose_player(user_input)
@@ -40,7 +44,7 @@ class CLI
             when user_input == "exit"
                puts "\n\nRemember, basketball head, ball is life.\n\n"
             else 
-               puts "You seem confused, bruh, how's a fresh start sound? Enter 'list'."
+               puts "\nYou seem confused, bruh! How's a fresh start sound? Enter 'start' to go back to the main menu or hit 'Exit' to leave!."
          end
       end   
    end
@@ -57,18 +61,44 @@ class CLI
       puts "\n<<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>>\n"
       puts " "
       puts "You picked #{player.name}."
-      puts "\n\n"
+      puts " "
       puts player.des
       puts " "
+      puts "Would you like to learn more about #{player.name} ? Enter 'y'."
       puts "\n<<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>>\n"
-      puts " "
-      puts player.bio
-      puts "\n\n\n"
+         input = gets.strip.downcase
+            if input == "y"
+               puts " "
+               puts player.bio
+            else
+               start
+            end
+      puts "\n"
          # binding.pry
-      puts "\n\n"
-         
+      puts "\n"    
    end
       
+   # def view_bio(index)
+   #    puts "\n<<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>><<<<>>>>\n"
+   #    puts " "
+   #    puts player.bio
+   #    puts "\n"
+   #       # binding.pry
+   #    puts "\n"
+
+   # end
+
+   def print_players(from_input)
+      puts ""
+      puts "---------- NBA Legends #{from_input} - #{from_input+9} ----------"
+      puts ""
+      Player.all[from_input-1, 10].each.with_index(from_input) do |player, index|
+        puts "#{index}. #{player.name}"
+      end  
+   end 
+#    puts "\n"
+#    Player.list_players
+# puts "\nSelect a player by number or enter 'exit' to leave at anytime:"
    
 end
 
--- lib/player.rb
+++ lib/player.rb
@@ -14,8 +14,8 @@ class Player
   
    def self.all
       @@all
+      # binding.pry
    end
-
 
    def self.list_players
       self.all.each_with_index do |player, i|
@@ -23,5 +23,25 @@ class Player
       end
    end
 
+   def self.all_names
+      @@all.collect {|player| player.name}
+   end
+
+   def create_shaq
+      self.all.each_with_index do |player, i|
+         if i == 32
+            player.name = "Shaquille O' Neal"
+            player.url = "https://en.wikipedia.org/wiki/Kazaam"
+            player.des = "The Big Cactus"
+            player.bio = "Legend."
+         end
+      end
+      # binding.pry
+   end
+
+
+ 
+
+   # a = Player.new("", "coll.com", "yep", "no")
 
 end
